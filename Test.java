6fcf0efaf7af912456485d22d0660541c5a2ee12
Alex Rodriguez <arodriguez99@protonmail.com>
2023-10-28 23:25:01 -0400

    switches from re.search() to re.finditer to find all matches in file

---
+++ Test.java
@@ -0,0 +1,14 @@
+public final double getBalanceDifference(Account a, Account b, boolean verbose) {
+}
+
+private static int getAge(User user) {
+}
+
+protected float getPI() {
+}
+
+public int addTwoNumbers(
+    int a,
+    int b
+    ) {
+}
